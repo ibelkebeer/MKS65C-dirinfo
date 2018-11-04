e2ec56657f1c336529f904cda41f18372fd906e6
Imad <ibelkebir@stuy.edu>
2018-11-04 18:02:18 -0500

    basic info

--- stat.c
+++ stat.c
@@ -3,29 +3,44 @@
 #include <time.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <string.h>
 
 int main(){
   DIR* d;
   d = opendir("dir");
   struct dirent* entry;
   int i = 0;
-  while(entry = readdir(d) && entry -> d_type == 4){
-    printf("%s\n", entry -> d_name);
+  while((entry = readdir(d))){
     i++;
   }
-  //char[256] dirs[i];
-  i = 0;
-  printf("_______________________");
-  if(entry){
-   printf("%s\n", entry -> d_name);
-   i++;
-  }
-  while(entry = readdir(d)){
-   printf("%s\n", entry -> d_name);
-   i++;
-  }
   closedir(d);
-  //d = opendir("dir");
-  //char
+  struct dirent* entries[i + 1];
+  entries[i] = 0;
+  d = opendir("dir");
+  i = 0;
+  while((entry = readdir(d))){
+    entries[i] = entry;
+    i++;
+  }
+  printf("\nStatistics for directory: %s\n", entries[0] -> d_name);
+  printf("Total directory size: %d Bytes\n", entries[0] -> d_reclen);
+  printf("\nDirectories:\n");
+  i = 0;
+  while(entries[i]){
+    if(entries[i] -> d_type == 4){
+      printf("%s\n", entries[i] -> d_name);
+    }
+    i++;
+  }
+  printf("\nRegular Files:\n");
+  i = 0;
+  while(entries[i]){
+    if(entries[i] -> d_type != 4){
+      printf("%s\n", entries[i] -> d_name);
+    }
+    i++;
+  }
+  printf("\n");
+  closedir(d);
   return 0;
 }
